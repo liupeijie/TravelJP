a1c4b85f683f30a0cc2e54918e98eb746c6cd45e
liupeijie <liupeijie525@gmail.com>
2015-10-11 19:13:09 +0900

    Initial Commit

---
+++ TravelJP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TravelJP
+//
+//  Created by 向日葵会下雨 on H27/10/11.
+//  Copyright © 平成27年 向日葵会下雨. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TravelJPTests/TravelJPTests.swift
@@ -0,0 +1,36 @@
+//
+//  TravelJPTests.swift
+//  TravelJPTests
+//
+//  Created by 向日葵会下雨 on H27/10/11.
+//  Copyright © 平成27年 向日葵会下雨. All rights reserved.
+//
+
+import XCTest
+@testable import TravelJP
+
+class TravelJPTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
