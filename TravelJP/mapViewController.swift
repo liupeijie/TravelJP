c389853d75186bd7197058705e25cca29336e91b
liupeijie <liupeijie525@gmail.com>
2015-10-17 00:39:58 +0900

    tab

---
+++ TravelJP/mapViewController.swift
@@ -0,0 +1,35 @@
+//
+//  mapViewController.swift
+//  TravelJP
+//
+//  Created by 向日葵会下雨 on H27/10/17.
+//  Copyright © 平成27年 向日葵会下雨. All rights reserved.
+//
+
+import UIKit
+
+class mapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
